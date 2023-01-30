d66b79b1148ffbaacadb224013086ae01e787c7c
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2023-01-30 13:23:24 +0000

    include notices in www to stg copy

--- bin/www2stg.sh
+++ bin/www2stg.sh
@@ -2,4 +2,9 @@
 # Copy production media and database to staging
 cd $(dirname "$0")
 ./media-www2stg.sh
+if [[ -f ~/public_html/wp-content/plugins/semla/core/notices.html ]]; then
+	cp ~/public_html/wp-content/plugins/semla/core/notices.html ~/public_html/sub/stg/wp-content/plugins/semla/core
+else
+	rm ~/public_html/sub/stg/wp-content/plugins/semla/core/notices.html
+fi
 ./db-www2stg.sh
