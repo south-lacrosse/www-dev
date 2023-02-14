52436affea00ac8525cf015556c0ae2d769bcaf5
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2023-02-14 10:22:39 +0000

    don't use JSON from another block

--- src/blocks/map/transforms.js
+++ src/blocks/map/transforms.js
@@ -1,17 +1,13 @@
-/**
- * WordPress dependencies
- */
 import { createBlock } from '@wordpress/blocks';
 import mapBlockMeta from './block.json';
-import locationBlockMeta from '../location/block.json';
 
 const transforms = {
 	to: [
 		{
 			type: 'block',
-			blocks: [ locationBlockMeta.name ],
+			blocks: [ 'semla/location' ],
 			transform: ( attributes, innerBlocks ) => {
-				return createBlock( locationBlockMeta.name, {}, [
+				return createBlock( 'semla/location', {}, [
 					createBlock( mapBlockMeta.name, attributes, innerBlocks ),
 				] );
 			},
