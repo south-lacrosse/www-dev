1cea6fd456e59832ae10987186439bffef0e0dab
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2023-04-22 06:55:28 +0100

    update node modules, improve node scripts
    - remove inline-source-cli as it uses outdated dependencies
    - update scripts to use process.stderr instead of console.log
    - scripts use environment vars directly instead of passing via command
       line

---
+++ src/scripts/inline-map-dialog.mjs
@@ -0,0 +1,32 @@
+/**
+ * Inline scripts and CSS into map dialog HTML
+ */
+
+import { inlineSource } from 'inline-source';
+import fs from 'fs';
+
+if (!process.env.npm_config_www) {
+	console.error('No www environment variable set in .npmrc');
+	process.exit(1);
+}
+const out =
+	process.env.npm_config_www +
+	'/wp-content/plugins/semla/modal/map-dialog.html';
+
+try {
+	const html = await inlineSource('src/blocks/map/map-dialog.html', {
+		compress: true,
+		rootpath: 'src/blocks/map/',
+	});
+	fs.writeFile(out, html, (err) => {
+		if (err) {
+			process.stderr.write(`Error: ${err}\n`);
+			return process.exit(1);
+		}
+		process.stderr.write(`Written to ${out}\n`);
+		process.exit(0);
+	});
+} catch (err) {
+	process.stderr.write(`Error: ${err}\n`);
+	process.exit(1);
+}
