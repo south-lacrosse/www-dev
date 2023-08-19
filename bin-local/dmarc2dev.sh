9165e984909fd49410b65f087fd5023d32b6dd40
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2023-08-19 11:39:34 +0100

    add script to copy dmarc tables to dev

---
+++ bin-local/dmarc2dev.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Load latest production DMARC tables to WordPress database
+
+if [[ $# -gt 1 ]]; then
+	echo "Usage: $0 [wp-dir]"
+	exit 1
+fi
+
+if [[ $# -eq 0 ]]; then
+# Find WordPress in current dir or ancestors
+	if [[ -f wp-config.php ]] ; then
+		wp=.
+	else
+		dir=$(realpath .)
+		wp=""
+		while [[ -z "$wp" && -d "$dir" ]] ; do
+			if [[ -f "$dir/wp-config.php" ]]; then
+				wp="$dir"
+			else
+				dir=${dir%/*}
+			fi
+		done
+		if [[ -z "$wp" ]] ; then
+			echo "Cannot find WordPress directory. Run with $0 <wp-dir>"
+			exit 1
+		fi
+		echo "Found WordPress in $wp"
+	fi
+else
+	if [[ ! -d "$1" ]] ; then
+		echo "$1 is not a directory."
+		exit 1
+	fi
+	wp=${@%/} # remove trailing slash
+	if [[ ! -f "$wp/wp-config.php" ]] ; then
+		echo "$1 is not a WordPress directory."
+		exit 1
+	fi
+fi
+
+cd $wp || exit 1
+latest_backup=$(ssh sl 'ls -t ~/public_html/bin/backups/dmarc*.sql.gz' | head -1)
+[[ -z "$latest_backup" ]] && exit
+scp "sl:$latest_backup" bin/backups/
+
+bin/run-sql.sh bin/backups/$(basename -- "$latest_backup") -y
