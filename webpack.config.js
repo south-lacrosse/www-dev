1f78d29511b23d262da5c7ee901f7584e87e65c8
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2023-09-18 20:15:38 +0100

    combine blocks and blocks-core builds
    - originally blocks and non-blocks had to be built separately, but now
      they can be combined into a single build

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+// Custom build to add extra endpoints for non blocks, i.e. modules which don't
+// have a block.json. This allows us to have one build, rather than having to
+// run wp-scripts separately for blocks and non-blocks.
+
+// Import the original config from the @wordpress/scripts package.
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+
+// Import the helper to find and generate the entry points in the src directory
+const { getWebpackEntryPoints } = require( '@wordpress/scripts/utils/config' );
+
+// Add any a new entry point by extending the webpack config.
+module.exports = {
+	...defaultConfig,
+	entry: {
+		...getWebpackEntryPoints(),
+		'core/index': './src/blocks/core/index.js',
+	},
+};
