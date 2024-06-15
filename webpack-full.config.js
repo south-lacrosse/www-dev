b13adfc1f098444b7a3196d16b3a5756016c7c4b
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2024-06-15 17:07:12 +0100

    update npm packages, wp-scripts to 27.9.0
    - see package.json diff for changes
    - update webpack config as method to modify entry points changed

--- webpack-full.config.js
+++ webpack-full.config.js
@@ -2,17 +2,18 @@
 // have a block.json. This allows us to have one build, rather than having to
 // run wp-scripts separately for blocks and non-blocks.
 
+// Important: changing the entry points isn't documented, and is subject
+// to change. If this config fails to compile all the modules check
+// @wordpress/scripts/config/webpack.config.js
+
 // Import the original config from the @wordpress/scripts package.
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
-
-// Import the helper to find and generate the entry points in the src directory
-const { getWebpackEntryPoints } = require( '@wordpress/scripts/utils/config' );
 
 // Add any a new entry point by extending the webpack config.
 module.exports = {
 	...defaultConfig,
 	entry: {
-		...getWebpackEntryPoints(),
+		...defaultConfig.entry(),
 		'core/index': './src/blocks/core/index.js',
 		'editor/index': './src/blocks/editor/index.js',
 	},
