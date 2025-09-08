c45a153a8d8e206f2836ddf0bcd54f1f315f7b92
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2025-09-08 11:45:54 +0100

    don't purge menu when copying db to staging
    - already done in load-production-backup.sh

--- bin/db-www2stg.sh
+++ bin/db-www2stg.sh
@@ -3,7 +3,5 @@
 ~/public_html/bin/full-backup.sh full-www.sql
 cd ~/public_html/sub/stg/
 ./bin/load-production-backup.sh ~/public_html/bin/backups/full-www.sql
-# need to purge menu cache in case it's changed in the DB
-wp purge menu
 echo Purging Litespeed Cache
 wp litespeed-purge all
