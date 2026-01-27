92e575bc069abb53c34657252ce04c291f0d53f4
Dave Slaughter <26726004+dave-slaughter@users.noreply.github.com>
2026-01-27 12:22:07 +0000

    enable xdebug extension in debug wp script

--- bin-local/wpd.bat
+++ bin-local/wpd.bat
@@ -1,5 +1,11 @@
 @echo off
-@rem Run a WP-CLI command in debug mode
+rem Run a WP-CLI command in debug mode, will add the xdebug extension if needed
 setlocal
 set XDEBUG_MODE=debug,develop
-php -dxdebug.start_with_request=yes "C:\Program Files (x86)\Local\resources\extraResources\bin\wp-cli\wp-cli.phar" %*
+>nul find "php_xdebug.dll" "%PHPRC%\php.ini" && (
+  set option=
+) || (
+  set option=-dzend_extension=php_xdebug.dll
+)
+php %option% -dxdebug.start_with_request=yes "C:\Program Files (x86)\Local\resources\extraResources\bin\wp-cli\wp-cli.phar" %*
+
